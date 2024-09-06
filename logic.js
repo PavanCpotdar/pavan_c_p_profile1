469f8c4270778d726099670722180ae48740d61b
PavanCpotdar <pavanc72@gmail.com>
2024-09-06 13:08:29 +0530

    About page added

--- logic.js
+++ logic.js
@@ -1,27 +1,51 @@
-function openHamburger(){
-    console.log("object")
-    const menu = document.querySelector('.hamburger-menu-icon');
-    const menuLinks = document.querySelector('.hamburger-menu-links');
+// on smaller window
 
-    menu.classList.toggle('open')
-    menuLinks.classList.toggle('open')
+function openHamburger() {
+  console.log("object");
+  const menu = document.querySelector(".hamburger-menu-icon");
+  const menuLinks = document.querySelector(".hamburger-menu-links");
+
+  menu.classList.toggle("open");
+  menuLinks.classList.toggle("open");
 }
 
-const menu = document.querySelector('.hamburger-menu-icon')
+const menu = document.querySelector(".hamburger-menu-icon");
 
-menu.addEventListener('click',openHamburger);
-
+menu.addEventListener("click", openHamburger);
 
 let button = document.querySelector("#submit-form");
 
-button.addEventListener('click',handleSubmit);
+button.addEventListener("click", handleSubmit);
 
-function handleSubmit (e){
-    e.preventDefault()
-    var link = "mailto:pavanc72@gmail.com"
-    + "?cc=pavanpotdar95@gmail.com"
-    + "&subject=" + encodeURIComponent("This is my subject")
-    + "&body=" + encodeURIComponent(document.getElementById('myText').value)
-;
-window.location.href = link;
+function handleSubmit(e) {
+  e.preventDefault();
+  var link =
+    "mailto:pavanc72@gmail.com" +
+    "?cc=pavanpotdar95@gmail.com" +
+    "&subject=" +
+    encodeURIComponent("This is my subject") +
+    "&body=" +
+    encodeURIComponent(document.getElementById("myText").value);
+  window.location.href = link;
 }
+
+// scroll btn
+
+let scrollBtn = document.querySelector(".scroll-btn");
+
+window.onscroll = () => scrollFunction();
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollBtn.style.display = "block";
+  } else {
+    scrollBtn.style.display = "none";
+  }
+}
+
+function scrollToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+scrollBtn.addEventListener("click", () => scrollToTop());
